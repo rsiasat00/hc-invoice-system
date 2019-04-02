ff28d2fed66894cb71d986e983381455c56f7737
Ralph Julian Siasat <rsiasat00@gmail.com>
2019-04-02 22:56:43 +0800

    - Added fillables on Product model class

--- app/Product.php
+++ app/Product.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $fillable = [
+        'name',
+        'price',
+        'tax'
+    ];
+
     public function orders() {
         return $this->hasMany('App\Order');
     }
